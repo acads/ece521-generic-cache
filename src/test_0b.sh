7c54f8300ccaeaab180639d39ed1e0a89a23554b
Aravindhan Dhanasekaran <adhanas@ncsu.edu>
2014-09-25 10:19:14 -0400

    Added testing automation script for 0b.

--- src/test_0b.sh
+++ src/test_0b.sh
@@ -17,44 +17,44 @@ no_color='\e[0m'
 
 function run_start()
 {
-    echo -e ${red_bold}
-    echo "Running sim_cache against TA given validation traces.. start"
-    echo -e ${no_color}
+    echo -e "${red_bold}Running sim_cache against TA given "    \
+        "validation traces.. start${no_color}"
 }
 
 
 function run_end()
 {
-    echo -e ${red_bold}
-    echo "Running sim_cache against TA given validation traces.. end"
-    echo "__________________________________________________________________________________"
-    echo -e ${no_color}
+    echo -e "${red_bold}Running sim_cache against TA given "    \
+        "validation traces.. end${no_color}"
 }
 
 
 function diff_start()
 {
-    echo -e ${red_bold}
-    echo "Diff'ing sim_cache output against TA given validation runs.. start"
-    echo -e ${no_color}
+    echo -e "${red_bold}Diff'ing sim_cache output against TA given "   \
+        "validation runs.. start${no_color}"
 }
 
 
 function diff_end()
 {
-    echo -e ${red_bold}
-    echo "Diff'ing sim_cache output against TA given validation runs.. end"
-    echo "__________________________________________________________________________________"
-    echo -e ${no_color}
+    echo -e "${red_bold}Diff'ing sim_cache output against TA given "    \
+        "validation runs.. end${no_color}"
 }
 
+
+function print_line()
+{
+    echo -e "${red_bold}-------------------------------------------------------------------${no_color}"
+    echo " "
+}
 
 # Check validation runs
 function run6()
 {
     run_start
-    echo "time ./sim_cache 16 16384 1 0 0 gcc_trace.txt > tmp1"
-    time ./sim_cache 16 16384 1 0 0 gcc_trace.txt > b6
+    echo "time ./sim_cache 32 2048 4 0 0 0 gcc_trace.txt > b6"
+    time ./sim_cache 32 2048 4 0 0 0 gcc_trace.txt > b6
     run_end
 
     echo " "
@@ -69,8 +69,8 @@ function run6()
 function run7()
 {
     run_start
-    echo "time ./sim_cache 128 2048 8 0 1 go_trace.txt > tmp2"
-    time ./sim_cache 128 2048 8 0 1 go_trace.txt > b7
+    echo "time ./sim_cache 16 1024 8 0 0 go_trace.txt > b7"
+    time ./sim_cache 16 1024 8 0 0 0 go_trace.txt > b7
     run_end
 
     echo " "
@@ -84,8 +84,8 @@ function run7()
 function run8()
 {
     run_start
-    echo "time ./sim_cache 32 4096 4 0 1 perl_trace.txt > tmp3"
-    time ./sim_cache 32 4096 4 0 1 perl_trace.txt > b8
+    echo "time ./sim_cache 32 1024 8 0 0 0 perl_trace.txt > b8"
+    time ./sim_cache 32 1024 8 0 0 0 perl_trace.txt > b8
     run_end
 
     echo " "
@@ -99,8 +99,8 @@ function run8()
 function run9()
 {
     run_start
-    echo "time ./sim_cache 64 8192 2 1 0 gcc_trace.txt > tmp4"
-    time ./sim_cache 64 8192 2 1 0 gcc_trace.txt > b9
+    echo "time ./sim_cache 128 1024 2 0 0 0 gcc_trace.txt > b9"
+    time ./sim_cache 128 1024 2 0 0 0 gcc_trace.txt > b9
     run_end
 
     echo " "
@@ -114,8 +114,8 @@ function run9()
 function run10()
 {
     run_start
-    echo "time ./sim_cache 32 1024 4 1 1 go_trace.txt > tmp5"
-    time ./sim_cache 32 1024 4 1 1 go_trace.txt > b10
+    echo "time ./sim_cache 64 8192 2 0 0 0 perl_trace.txt > b10"
+    time ./sim_cache 64 8192 2 0 0 0 perl_trace.txt > b10
     run_end
 
     echo " "
@@ -128,13 +128,18 @@ function run10()
 
 # Main driver
 # Call the appropriate test function based on passed input
-if ["$1" = "all" ]
+if [ "$1" = "all" ]
 then
     run6
+    print_line
     run7
+    print_line
     run8
+    print_line
     run9
+    print_line
     run10
+    print_line
 fi
 
 case "$1" in
